044eec6c524e3915872c8423e6330c2d465e7214
yasminegamal19 <yasminegamal2222@gmail.com>
2025-12-11 10:33:35 +0200

    new update

--- src/Shared/Components/Heading/Heading.jsx
+++ src/Shared/Components/Heading/Heading.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import './Heading.modules.css'
+import React from "react";
+import "./Heading.modules.css";
 
 const Heading = () => {
-    return (
-        <div className='heading py-5'>
-            <div class="overlay overlay-bg"></div>
-            <div className="container">
-                <div className="content">
-                <h1>We’re Industrial solution</h1>
-                <p className='text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et <br></br>dolore magna aliqua. Ut enim ad minim. sed do eiusmod tempor incididunt.</p>
-                <button className='btn text-uppercase'>get started</button>
-            </div>
+  return (
+    <div className="heading py-5">
+      <div class="overlay overlay-bg"></div>
+      <div className="container">
+        <div className="content">
+          <h1>We’re Industrial test solution</h1>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et <br></br>dolore magna aliqua.
+            Ut enim ad minim. sed do eiusmod tempor incididunt.
+          </p>
+          <button className="btn text-uppercase">get started</button>
         </div>
-        </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Heading;
